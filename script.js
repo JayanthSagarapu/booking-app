57fb5a60b1d9595ff5f2751e219ada84219f693d
JayanthSagarapu <sjayanthkumar1@gmail.com>
2023-04-13 15:42:31 +0530

    Delete buttons and remove from local storage

--- script.js
+++ script.js
@@ -1,28 +1,66 @@
 const username = document.querySelector('#name');
 const email = document.querySelector('#email');
 const phone = document.querySelector('#phone');
-const form = document.querySelector('#form');
-const list = document.querySelector('#list')
-const btn = document.querySelector('.btn')
+const form = document.getElementById('form');
+const list = document.getElementById('list');
+const submitbtn = document.getElementById('submit');
+const deletebtn = document.getElementById('del');
 
-btn.addEventListener('click',onsubmit);
+submitbtn.addEventListener('click',addItem) ;
 
-function onsubmit(e){
-    e.preventDefault();
+deletebtn.addEventListener('click',deletelastItem);
 
+
+
+function addItem(e){
+    e.preventDefault()
     const nameInput = username.value;
     const emailInput = email.value;
     const phoneInput = phone.value;
 
+    const obj={
+        name : nameInput,
+        email : emailInput,
+        phone : phoneInput,
+    }
+    
+    //new li element
     const li = document.createElement('li');
 
     li.className = 'list-group-item align-self-center w-75 mb-1 bg-secondary';
 
-    li.append(document.createTextNode(nameInput)," - ",document.createTextNode(emailInput)," - ",document.createTextNode(phoneInput))
+    li.textContent=nameInput + " - " + emailInput + " - " + phoneInput;
 
-    list.appendChild(li)
+    
+    //adding to local storage as an object
+     localStorage.setItem(obj.name ,JSON.stringify(obj))
 
-    localStorage.setItem(nameInput ,JSON.stringify({name:nameInput,email:emailInput,number:phoneInput}))
+    //add delete button to listitems
+    const delbtn = document.createElement('button');
 
+    delbtn.className = 'btn btn-sm float-right delete';
+    
+    delbtn.textContent='Delete';
+
+    delbtn.addEventListener('click',removelistitem)
+    function removelistitem(){
+        localStorage.removeItem(obj.name)
+        list.removeChild(li)
+    };
+    
+    li.appendChild(delbtn);
+    
+    list.appendChild(li);
 
 }
+
+
+function deletelastItem(e){
+    e.preventDefault();
+
+    const last = list.lastChild;
+    list.removeChild(last);
+}
+
+
+
